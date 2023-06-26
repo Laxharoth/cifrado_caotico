b5ce84e9ef95d1ed5b4dfa6b1db67bd744761a68
Laxharoth <zeydenlaxharoth@gmail.com>
2023-06-26 15:46:45 -0600

    Config structure update

--- ConfigStructure.c
+++ ConfigStructure.c
@@ -1,19 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "ConfigStructure.h"
 
-#define MAX_LINE_LENGTH 100
-
-typedef struct {
-    int lambda;
-    int beta1;
-} Configuracion;
-
-void readConfigFile(const char *filename, Configuracion *config) {
+Configuracion readConfigFile(const char *filename) {
+    Configuracion config;
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
         printf("No se pudo abrir el archivo de configuración.\n");
-        return;
+        return config;
     }
 
     char line[MAX_LINE_LENGTH];
@@ -34,6 +26,7 @@ void readConfigFile(const char *filename, Configuracion *config) {
             '\0';  // Reemplazar el delimitador por un terminador de cadena
         char *key = line;
         char *value = delimiter + 1;
+        char *endptr;
 
         // Eliminar espacios en blanco alrededor de la clave y el valor
         char *end = value + strlen(value) - 1;
@@ -45,12 +38,18 @@ void readConfigFile(const char *filename, Configuracion *config) {
         }
 
         // Guardar la clave y el valor en la estructura
-        if (strcmp(key, "lambda") == 0) {
-            config->lambda = atoi(value);
-        } else if (strcmp(key, "beta1") == 0) {
-            config->beta1 = atoi(value);
+        if (strcmp(key, "beta") == 0) {
+            config.beta = atoi(value);
+        } else if (strcmp(key, "lambda") == 0) {
+            config.lambda = atoi(value);
+        } else if (strcmp(key, "seed") == 0) {
+            config.seed = strtoull(value, &endptr, 10);;
+        } else if (strcmp(key, "file_size") == 0) {
+            config.file_size = strtoull(value, &endptr, 10);;
         }
     }
 
     fclose(file);
+
+    return config;
 }
---
+++ ConfigStructure.h
@@ -0,0 +1,19 @@
+#ifndef __CONFIGSTRUCTURE_H__
+#define __CONFIGSTRUCTURE_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 100
+
+typedef struct {
+    int beta;
+    int lambda;
+    unsigned long long seed;
+    unsigned long long file_size;
+} Configuracion;
+
+Configuracion readConfigFile(const char *filename);
+
+#endif

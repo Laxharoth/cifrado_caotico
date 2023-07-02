78197f771db6136928b23a6da5bd0886b193f723
Luis Daniel Espino Mandujano <zeydenlaxharoth@gmail.com>
2023-07-02 12:45:09 -0600

    Update write_rng.c

--- write_rng.c
+++ write_rng.c
@@ -7,7 +7,6 @@
 #include "ConfigStructure.h"
 #include "fn.c"
 
-
 void calcular_hash(const unsigned char *data, size_t data_length,
                    int output_length, unsigned char *hash_value) {
     EVP_MD_CTX *mdctx;
@@ -32,9 +31,9 @@ void generate_random_file_2(const char *file_path,
         return;
     }
 
-    ull X[10];
+    ull X[NUMBER_OF_CAHOTIC_MAPS];
     calcular_hash((unsigned char *)&(config->seed), sizeof(ull),
-                  sizeof(ull) * 10, (unsigned char *)X);
+                  sizeof(ull) * NUMBER_OF_CAHOTIC_MAPS, (unsigned char *)X);
     ull Yi;
 
     const size_t chunk_size = sizeof(ull);
@@ -44,8 +43,40 @@ void generate_random_file_2(const char *file_path,
         size_t chunk_bytes =
             (chunk_size < remaining_bytes) ? chunk_size : remaining_bytes;
 
-        Yi = renyi_array_2(X, config->beta, config->lambda);
+        Yi = renyi_array_combine_maps_with_mask_and_replace(X, config->beta,
+                                                            config->lambda);
         // Yi = renyi_array_generator(X);
+
+        fwrite(&Yi, sizeof(unsigned char), chunk_bytes, file);
+        remaining_bytes -= chunk_bytes;
+    }
+
+    fclose(file);
+}
+
+void generate_random_file_3(const char *file_path,
+                            const Configuracion *config) {
+    typedef unsigned long long ull;
+    FILE *file = fopen(file_path, "wb");
+    if (file == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
+
+    ull X[NUMBER_OF_CAHOTIC_MAPS];
+    calcular_hash((unsigned char *)&(config->seed), sizeof(ull),
+                  sizeof(ull) * NUMBER_OF_CAHOTIC_MAPS, (unsigned char *)X);
+    ull Yi;
+
+    const size_t chunk_size = sizeof(ull);
+    size_t remaining_bytes = config->file_size;
+    while (remaining_bytes > 0) {
+        // Generar datos aleatorios para el fragmento actual
+        size_t chunk_bytes =
+            (chunk_size < remaining_bytes) ? chunk_size : remaining_bytes;
+
+        Yi = renyi_array_random_byte_select_with_replace(X, config->beta,
+                                                         config->lambda);
 
         fwrite(&Yi, sizeof(unsigned char), chunk_bytes, file);
         remaining_bytes -= chunk_bytes;
@@ -98,6 +129,10 @@ int main() {
     {
         const char *file_path = "random_data2.bin";
         generate_random_file_2(file_path, &config);
+    }
+    {
+        const char *file_path = "random_data3.bin";
+        generate_random_file_3(file_path, &config);
     }
     return 0;
 }

2c6af90fc41f5a2be31b95d1cb471cc674857b4e
Luis Daniel Espino Mandujano <zeydenlaxharoth@gmail.com>
2023-10-05 19:11:05 -0600

    Update renyi_map_trayectory.py

--- renyi_map_trayectory.py
+++ renyi_map_trayectory.py
@@ -1,14 +1,25 @@
 import os
 import struct
 from dataclasses import dataclass
-from typing import List
+from typing import List, Callable, Any
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
+from numpy.typing import NDArray
 
 ull = np.uint64
 byte = np.uint8
+
+
+def find_most_significant_bit(value: ull) -> ull:
+    msb: ull = ull(0)
+    one: ull = ull(1)
+    while value > 1:
+        value >>= one
+        msb += one
+
+    return msb
 
 
 @dataclass
@@ -18,18 +29,14 @@ class LogisticConstants:
     r: ull
 
 
-logistic_constants = {
-    1: LogisticConstants(max_x=ull(2**33 - 1), t=ull(2**33), r=ull(1)),
-    2: LogisticConstants(max_x=ull(2**32 - 1), t=ull(2**32), r=ull(2)),
-    3: LogisticConstants(max_x=ull(2**32 - 1), t=ull(2**32), r=ull(3)),
-    4: LogisticConstants(max_x=ull(2**32 - 1), t=ull(2**32), r=ull(4)),
-    5: LogisticConstants(max_x=ull(2**31 - 1), t=ull(2**31), r=ull(5)),
-    6: LogisticConstants(max_x=ull(2**31 - 1), t=ull(2**31), r=ull(6)),
-    7: LogisticConstants(max_x=ull(2**31 - 1), t=ull(2**31), r=ull(7)),
-    8: LogisticConstants(max_x=ull(2**31 - 1), t=ull(2**31), r=ull(8)),
-    9: LogisticConstants(max_x=ull(2**31 - 1), t=ull(2**31), r=ull(9)),
-    10: LogisticConstants(max_x=ull(2**31 - 1), t=ull(2**31), r=ull(10)),
-}
+MAX_ULL = 0xFFFFFFFFFFFFFFFF
+
+logistic_constants: dict[int, LogisticConstants] = {}
+for r in range(1, 11):
+    t = ull(2) * ull(np.sqrt(MAX_ULL / r))
+    max_x = (ull(1) << find_most_significant_bit(t)) - ull(1)
+
+    logistic_constants[r] = LogisticConstants(max_x=max_x, t=t, r=ull(r))
 
 
 def read_ull(filename: str) -> List[ull]:
@@ -67,8 +74,40 @@ def main():
             plt.cla()
 
 
-def logistic_map(x: ull, r: byte):
+def logistic_map(x: NDArray[np.floating[Any]], r: byte):
     return r * x * (logistic_constants[r].t - x)
+
+
+def circle_map(x: NDArray[np.floating[Any]]):
+    a = (2**64 - 1) / 2
+    b = (2**64 - 1) * 3 / 8
+    r = (2**64 - 1) * 5 / 8
+    return np.sqrt(r**2 - (x - a) ** 2) + b
+
+
+def graficar_funcion(
+    func: Callable[[NDArray[np.floating[Any]]], float],
+    start: float,
+    end: float,
+    num_puntos: int,
+):
+    """_summary_
+
+    Args:
+        f (Callable[[float], float]): _description_
+        a (float): _description_
+        b (float): _description_
+        num_puntos (float): _description_
+    """
+    x_val = np.linspace(start, end, num_puntos)
+    y_val = func(x_val)
+
+    plt.plot(x_val, y_val)
+    plt.xlabel("x")
+    plt.ylabel("f(x)")
+    plt.title("Gráfica de f(x)")
+    plt.grid(True)
+    plt.show()
 
 
 def logistic_map_test():
@@ -77,23 +116,19 @@ def logistic_map_test():
 
     for r in range(1, 11):
         x = ull(1)
-        identity = [
-            [ull(0), ull(0)],
-            [ull(1), ull(1)],
-        ]
-        for _ in range(255):
+        for _ in range(100):
             X.append(x)
             x = logistic_map(X[-1], byte(r))
             Y.append(x)
             x = x & logistic_constants[r].max_x
             X.append(x)
             Y.append(x)
-        # plt.xlim(logistic_constants[r].max_x)
-        # plt.ylim(logistic_constants[r].t)
+        plt.xlim((0, logistic_constants[r].max_x))
+        plt.ylim((0, 0xFFFFFFFFFFFFFFFF))
         X = np.array(X)
         Y = np.array(Y)
 
-        plt.plot(X, Y)
+        plt.plot(X, Y, c="blue")
         plt.savefig(f"img/logistic/r={r}.png")
         X = []
         Y = []
@@ -101,4 +136,4 @@ def logistic_map_test():
 
 
 if __name__ == "__main__":
-    logistic_map_test()
+    graficar_funcion(circle_map, 0, (2**64), 1000)

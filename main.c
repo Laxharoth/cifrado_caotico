bff2e4a93cedbd668a90437f4d43b00696eacf2b
Laxharoth <zeydenlaxharoth@gmail.com>
2023-11-07 15:51:03 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#include <openssl/evp.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,20 +9,13 @@
 
 #define SAMPLE_SIZE 1
 #define MAX_ULL 0xFFFFFFFFFFFFFFFF
-void calcular_hash(const unsigned char *data, size_t data_length,
-                   int output_length, unsigned char *hash_value) {
-    EVP_MD_CTX *mdctx;
-    const EVP_MD *md;
 
-    mdctx = EVP_MD_CTX_new();
-    md = EVP_sha3_512();
-
-    EVP_DigestInit_ex(mdctx, md, NULL);
-    EVP_DigestUpdate(mdctx, data, data_length);
-    EVP_DigestFinal_ex(mdctx, hash_value, NULL);
-
-    EVP_MD_CTX_free(mdctx);
-}
+/*
+    Las funciones generate_random_file
+        de 1-3 utilizan arreglos para varias condiciones iniciales
+        
+        los de 4 - 7 utilizan  solo un valor como condicion inicial
+*/
 
 void generate_random_file_1(const char *file_path,
                             const Configuracion *config) {
@@ -31,11 +25,11 @@ void generate_random_file_1(const char *file_path,
         printf("No se pudo abrir el archivo.\n");
         return;
     }
-
+    // Obtener condiciones iniciales
     ull X[NUMBER_OF_CAHOTIC_MAPS];
-    ull Yi;
-    calcular_hash((unsigned char *)&(config->seed), sizeof(ull),
-                  sizeof(ull) * NUMBER_OF_CAHOTIC_MAPS, (unsigned char *)X);
+    for(size_t i = 0; i < NUMBER_OF_CAHOTIC_MAPS * 2; ++i){
+        ((uint32_t*)X)[i] = rand();
+    }
 
     const size_t chunk_size =
         sizeof(ull) *
@@ -64,10 +58,11 @@ void generate_random_file_2(const char *file_path,
         printf("No se pudo abrir el archivo.\n");
         return;
     }
-
+    // Obtener condiciones iniciales
     ull X[NUMBER_OF_CAHOTIC_MAPS];
-    calcular_hash((unsigned char *)&(config->seed), sizeof(ull),
-                  sizeof(ull) * NUMBER_OF_CAHOTIC_MAPS, (unsigned char *)X);
+    for(size_t i = 0; i < NUMBER_OF_CAHOTIC_MAPS * 2; ++i){
+        ((uint32_t*)X)[i] = rand();
+    }
     ull Yi;
 
     const size_t chunk_size = sizeof(ull);
@@ -96,10 +91,11 @@ void generate_random_file_3(const char *file_path,
         printf("No se pudo abrir el archivo.\n");
         return;
     }
-
+    // Obtener condiciones iniciales
     ull X[NUMBER_OF_CAHOTIC_MAPS];
-    calcular_hash((unsigned char *)&(config->seed), sizeof(ull),
-                  sizeof(ull) * NUMBER_OF_CAHOTIC_MAPS, (unsigned char *)X);
+    for(size_t i = 0; i < NUMBER_OF_CAHOTIC_MAPS * 2; ++i){
+        ((uint32_t*)X)[i] = rand();
+    }
     ull Yi;
 
     const size_t chunk_size = sizeof(ull);
@@ -130,7 +126,6 @@ void generate_random_file_4(const char *file_path,
     }
 
     srand(config->seed);
-
     ull X = rand();
     X = (X<<32)|rand();
 
@@ -138,7 +133,7 @@ void generate_random_file_4(const char *file_path,
         sizeof(ull);  // Tamaño de cada fragmento a escribir
     size_t remaining_bytes = config->file_size;
     ull divisor = config->r;
-    ull t = (0xFFFFFFFFFFFFFFFF / divisor);
+    ull t = (MAX_ULL / divisor);
     t = sqrtull(t);
     while (remaining_bytes > 0) {
         // Generar datos aleatorios para el fragmento actual
@@ -175,7 +170,7 @@ void generate_random_file_5(const char *file_path,
     const size_t chunk_size =
         sizeof(ull) * size;  // Tamaño de cada fragmento a escribir
     size_t remaining_bytes = config->file_size;
-    ull t = (0xFFFFFFFFFFFFFFFF / config->r);
+    ull t = (MAX_ULL / config->r);
     t = sqrtull(t);
     while (remaining_bytes > 0) {
         // Generar datos aleatorios para el fragmento actual
@@ -211,7 +206,7 @@ void generate_random_file_6(const char *file_path,
 
     ull divisor = config->r;
     size_t remaining_bytes = config->file_size;
-    ull t = (0xFFFFFFFFFFFFFFFF / divisor);
+    ull t = (MAX_ULL / divisor);
     t = sqrtull(t);
     while (remaining_bytes > 0) {
         const ull size = (X & mask)| 1; 
@@ -249,7 +244,7 @@ void generate_random_file_7(const char *file_path,
         sizeof(ull);  // Tamaño de cada fragmento a escribir
     size_t remaining_bytes = config->file_size;
     ull divisor = config->r;
-    ull t = (0xFFFFFFFFFFFFFFFF / divisor);
+    ull t = (MAX_ULL / divisor);
     t = sqrtull(t);
     while (remaining_bytes > 0) {
         // Generar datos aleatorios para el fragmento actual

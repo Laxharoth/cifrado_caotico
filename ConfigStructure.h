d98e33bdf0630f0ad6953cff4de1865a06a7fa0d
Laxharoth <zeydenlaxharoth@gmail.com>
2023-11-07 15:37:10 -0600

    Update ConfigStructure.h

--- ConfigStructure.h
+++ ConfigStructure.h
@@ -8,15 +8,20 @@
 #define MAX_LINE_LENGTH 100
 
 typedef struct {
+    // parametros de renyi
     unsigned long long beta;
     unsigned long long lambda;
-    unsigned long long bulk_size;
+    // parametro de logistico
     unsigned long long r;
+    // parametros de parabola
     unsigned long long h;
     unsigned long long k;
     unsigned long long n;
+    // semilla aleatoria
     unsigned long long seed;
+    // numero de bytes a escribir
     unsigned long long file_size;
+    unsigned long long bulk_size;
 } Configuracion;
 
 Configuracion readConfigFile(const char *filename);

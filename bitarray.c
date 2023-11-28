614d3720ed6d5ba19287029a0dbd2167587ad422
Luis Daniel Espino Mandujano <zeydenlaxharoth@gmail.com>
2023-11-27 20:11:20 -0600

    Create bitarray.c

---
+++ bitarray.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void readBitArrayFromFile(const char *filename, char *bitarray,
+                          size_t max_size) {
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        perror("Error al abrir el archivo");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t i = 0;
+    int c;
+    unsigned char one_mask = ((unsigned char)1);
+    while ((c = fgetc(file)) != EOF && i < max_size) {
+        if (c == '0') {
+            bitarray[(i >> 3)] &= ~((one_mask) << (i & 7));
+            i++;
+        }
+        if (c == '1') {
+            bitarray[(i >> 3)] |= (one_mask) << (i & 7);
+            i++;
+        }
+    }
+
+    fclose(file);
+}
+
+char ON(const char *bitarray, size_t bit) {
+    return bitarray[bit >> 3] & (1 << (bit & 7));
+}

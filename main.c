91d9e33b1fd8309daacbf484aac61df04dd31a52
Laxharoth <zeydenlaxharoth@gmail.com>
2023-11-30 10:31:50 -0600

    Update main.c

--- main.c
+++ main.c
@@ -274,13 +274,13 @@ void generate_random_file_8(const char *file_path,
     }
     const size_t numeroMapas = 4;
 
-    ull Xn[numeroMapas], parametros[numeroMapas], rng_generated_buffer = 65535,
-                                                  j = 5, H = 0;
+    ull Xn[numeroMapas], parametros[numeroMapas], epsilon = 65535, j = 5, H = 0;
 
+    srand(config->seed);
     for (size_t i = 0; i < numeroMapas; i++) {
-        srand(config->seed);
         Xn[i] = rand();
         Xn[i] = (Xn[i] << 32) | rand();
+        parametros[i] = (rand() % 3000) + 5;
     }
 
     const size_t chunk_size =
@@ -291,7 +291,7 @@ void generate_random_file_8(const char *file_path,
         size_t chunk_bytes =
             (chunk_size < remaining_bytes) ? chunk_size : remaining_bytes;
         SecureReal_TimeChaoticPartialEncryptionGenerator(
-            Xn, parametros, j, rng_generated_buffer, &H, numeroMapas);
+            Xn, parametros, j, epsilon, &H, numeroMapas);
 
 #ifndef MEASURE_TIME_ONLY
         fwrite(Xn, sizeof(unsigned char), chunk_bytes, file);
@@ -324,7 +324,7 @@ void generate_random_file_9(const char *file_path,
         // Generar datos aleatorios para el fragmento actual
         size_t chunk_bytes =
             (chunk_size < remaining_bytes) ? chunk_size : remaining_bytes;
-        rng_generated_buffer[index++] =
+        rng_generated_buffer[index++] = X =
             RenyiMap(X, config->beta, config->lambda);
 
         remaining_bytes -= chunk_bytes;
@@ -354,15 +354,14 @@ void generate_random_file_10(const char *file_path,
     const size_t chunk_size =
         sizeof(ull);  // Tamaño de cada fragmento a escribir
     size_t remaining_bytes = config->file_size;
-    ull divisor = config->r;
-    ull t = (MAX_ULL / divisor);
+    ull t = (MAX_ULL / config->r);
     t = sqrtull(t);
     ull index = 0;
     while (remaining_bytes > 0) {
         // Generar datos aleatorios para el fragmento actual
         size_t chunk_bytes =
             (chunk_size < remaining_bytes) ? chunk_size : remaining_bytes;
-        rng_generated_buffer[index++] = LogisticMapInt(X, config->r, t);
+        rng_generated_buffer[index++] = X = LogisticMapInt(X, config->r, t);
 
         remaining_bytes -= chunk_bytes;
     }
@@ -384,16 +383,17 @@ void generate_random_file_11(const char *file_path,
     }
     const size_t numeroMapas = 4;
     ull *rng_generated_buffer = (ull *)malloc(config->file_size);
-    ull Xn[numeroMapas], parametros[numeroMapas], epsilon = 65535, j = 5, H = 0;
+    ull Xn[numeroMapas], parametros[numeroMapas], j = 5;
 
+    srand(config->seed);
     for (size_t i = 0; i < numeroMapas; i++) {
-        srand(config->seed);
         Xn[i] = rand();
         Xn[i] = (Xn[i] << 32) | rand();
+        parametros[i] = (rand() % 3000) + 5;
     }
 
     const size_t chunk_size =
-        sizeof(ull) * numeroMapas;  // Tamaño de cada fragmento a escribir
+        sizeof(ull);  // Tamaño de cada fragmento a escribir
     size_t remaining_bytes = config->file_size;
     ull position = 0;
     ull index = 0;
@@ -402,6 +402,48 @@ void generate_random_file_11(const char *file_path,
         size_t chunk_bytes =
             (chunk_size < remaining_bytes) ? chunk_size : remaining_bytes;
         rng_generated_buffer[index++] = random_select_coupled_chaotic_map(
+            &position, Xn, parametros, j, 0b111);
+
+        remaining_bytes -= chunk_bytes;
+    }
+#ifndef MEASURE_TIME_ONLY
+    fwrite(rng_generated_buffer, sizeof(unsigned char), config->file_size,
+           file);
+#endif
+
+    free(rng_generated_buffer);
+    fclose(file);
+}
+
+void generate_random_file_12(const char *file_path,
+                             const Configuracion *config) {
+    typedef unsigned long long ull;
+    FILE *file = fopen(file_path, "wb");
+    if (file == NULL) {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
+    const size_t numeroMapas = 4;
+    ull *rng_generated_buffer = (ull *)malloc(config->file_size);
+    ull Xn[numeroMapas], parametros[numeroMapas], epsilon = 65535, j = 5, H = 0;
+
+    srand(config->seed);
+    for (size_t i = 0; i < numeroMapas; i++) {
+        Xn[i] = rand();
+        Xn[i] = (Xn[i] << 32) | rand();
+        parametros[i] = (rand() % 3000) + 5;
+    }
+
+    const size_t chunk_size =
+        sizeof(ull);  // Tamaño de cada fragmento a escribir
+    size_t remaining_bytes = config->file_size;
+    ull position = 0;
+    ull index = 0;
+    while (remaining_bytes > 0) {
+        // Generar datos aleatorios para el fragmento actual
+        size_t chunk_bytes =
+            (chunk_size < remaining_bytes) ? chunk_size : remaining_bytes;
+        rng_generated_buffer[index++] = random_select_coupled_chaotic_map_with_perturbation(
             &position, Xn, parametros, j, epsilon, &H, 0b111);
 
         remaining_bytes -= chunk_bytes;
@@ -415,6 +457,7 @@ void generate_random_file_11(const char *file_path,
     fclose(file);
 }
 
+
 int main() {
     const Configuracion config = readConfigFile("config.txt");
     char bitarray[10];
@@ -422,70 +465,76 @@ int main() {
     printf("%d\n", bitarray[0]);
     printf("%d\n", bitarray[1]);
 
-    if (ON(bitarray, 0)) {
+    if (ON(bitarray, 1)) {
         print_time({
             const char *file_path = "random_data1.bin";
             generate_random_file_1(file_path, &config);
         });
     }
-    if (ON(bitarray, 1)) {
+    if (ON(bitarray, 2)) {
         print_time({
             const char *file_path = "random_data2.bin";
             generate_random_file_2(file_path, &config);
         });
     }
-    if (ON(bitarray, 2)) {
+    if (ON(bitarray, 3)) {
         print_time({
             const char *file_path = "random_data3.bin";
             generate_random_file_3(file_path, &config);
         });
     }
-    if (ON(bitarray, 3)) {
+    if (ON(bitarray, 4)) {
         print_time({
             const char *file_path = "random_data4.bin";
             generate_random_file_4(file_path, &config);
         });
     }
-    if (ON(bitarray, 4)) {
+    if (ON(bitarray, 5)) {
         print_time({
             const char *file_path = "random_data5.bin";
             generate_random_file_5(file_path, &config);
         });
     }
-    if (ON(bitarray, 5)) {
+    if (ON(bitarray, 6)) {
         print_time({
             const char *file_path = "random_data6.bin";
             generate_random_file_6(file_path, &config);
         });
     }
-    if (ON(bitarray, 6)) {
+    if (ON(bitarray, 7)) {
         print_time({
             const char *file_path = "random_data7.bin";
             generate_random_file_7(file_path, &config);
         });
     }
-    if (ON(bitarray, 7)) {
+    if (ON(bitarray, 8)) {
         print_time({
             const char *file_path = "random_data8.bin";
             generate_random_file_8(file_path, &config);
         });
     }
-    if (ON(bitarray, 8)) {
+    if (ON(bitarray, 9)) {
         print_time({
             const char *file_path = "random_data9.bin";
             generate_random_file_9(file_path, &config);
         });
     }
-    if (ON(bitarray, 9)) {
+    if (ON(bitarray, 10)) {
         print_time({
             const char *file_path = "random_data10.bin";
             generate_random_file_10(file_path, &config);
         });
     }
-    if (ON(bitarray, 10)) {
+    if (ON(bitarray, 11)) {
         print_time({
             const char *file_path = "random_data11.bin";
             generate_random_file_11(file_path, &config);
+        });
+    }
+    if (ON(bitarray, 12)) {
+        print_time({
+            const char *file_path = "random_data11.bin";
+            generate_random_file_12(file_path, &config);
         });
     }
     return 0;

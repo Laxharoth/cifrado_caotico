7fc80192b8defbf7299a9d1e5bda378dbaaea6fa
Laxharoth <zeydenlaxharoth@gmail.com>
2024-02-04 10:16:55 -0600

    extracted generatos

---
+++ generators.h
@@ -0,0 +1,36 @@
+#ifndef GENERATORS_H
+#define GENERATORS_H
+#include "ConfigStructure.h"
+#include "fn.h"
+
+void generate_random_file_1(unsigned char *const buffer,
+                            const Configuracion *config);
+void generate_random_file_2(unsigned char *const buffer,
+                            const Configuracion *config);
+void generate_random_file_3(unsigned char *const buffer,
+                            const Configuracion *config);
+void generate_random_file_4(unsigned char *const buffer,
+                            const Configuracion *config);
+void generate_random_file_5(unsigned char *const buffer,
+                            const Configuracion *config);
+void generate_random_file_6(unsigned char *const buffer,
+                            const Configuracion *config);
+void generate_random_file_7(unsigned char *const buffer,
+                            const Configuracion *config);
+void generate_random_file_8(unsigned char *const buffer,
+                            const Configuracion *config);
+void generate_random_file_9(unsigned char *const buffer,
+                            const Configuracion *config);
+void generate_random_file_10(unsigned char *const buffer,
+                             const Configuracion *config);
+void generate_random_file_11(unsigned char *const buffer,
+                             const Configuracion *config);
+void generate_random_file_12(unsigned char *const buffer,
+                             const Configuracion *config);
+void generate_random_file_13(unsigned char *const buffer,
+                             const Configuracion *config);
+void generate_random_file_14(unsigned char *const buffer,
+                             const Configuracion *config);
+void generate_random_file_15(unsigned char *const buffer,
+                             const Configuracion *config);
+#endif  // GENERATORS_H

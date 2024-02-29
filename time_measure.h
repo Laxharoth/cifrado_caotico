aab09ba4782178202e59fa65601ac5bc2cab3a75
Laxharoth <zeydenlaxharoth@gmail.com>
2024-02-29 13:00:30 -0600

    Update time_measure.h
    Randomize macro variables names

--- time_measure.h
+++ time_measure.h
@@ -3,22 +3,28 @@
 
 #include <sys/types.h>
 #include <time.h>
+#ifndef SAMPLE_SIZE
+#define SAMPLE_SIZE 1
+#endif
 
-#define print_time(code)                                   \
-    {                                                      \
-        struct timespec start, end;                        \
-        double elapsed;                                    \
-        double average_time = 0;                           \
-        for (size_t i = 0; i < SAMPLE_SIZE; i++) {         \
-            clock_gettime(CLOCK_MONOTONIC, &start);        \
-            code;                                          \
-            clock_gettime(CLOCK_MONOTONIC, &end);          \
-            elapsed = (end.tv_sec - start.tv_sec) +        \
-                      (end.tv_nsec - start.tv_nsec) / 1e9; \
-            average_time += elapsed;                       \
-        }                                                  \
-        printf("Tiempo de ejecucion: %.9f segundos\n",     \
-               average_time / SAMPLE_SIZE);                \
+#define print_time(code)                                                    \
+    {                                                                       \
+        struct timespec start_0f915e730828c, end_0f915e730828c;             \
+        double elapsed_0f915e730828c;                                       \
+        double average_time_0f915e730828c = 0;                              \
+        for (size_t index_0f915e730828c = 0;                                \
+             index_0f915e730828c < SAMPLE_SIZE; index_0f915e730828c++) {    \
+            clock_gettime(CLOCK_MONOTONIC, &start_0f915e730828c);           \
+            code;                                                           \
+            clock_gettime(CLOCK_MONOTONIC, &end_0f915e730828c);             \
+            elapsed_0f915e730828c =                                         \
+                (end_0f915e730828c.tv_sec - start_0f915e730828c.tv_sec) +   \
+                (end_0f915e730828c.tv_nsec - start_0f915e730828c.tv_nsec) / \
+                    1e9;                                                    \
+            average_time_0f915e730828c += elapsed_0f915e730828c;            \
+        }                                                                   \
+        printf("Tiempo de ejecucion: %.9f segundos\n",                      \
+               average_time_0f915e730828c / SAMPLE_SIZE);                   \
     }
 
 #endif /* TIME_MEASURE_H */

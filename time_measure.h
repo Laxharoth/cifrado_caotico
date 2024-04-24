02d67cc556fa3ed31d2474e82d6d2fe2241c341f
Laxharoth <zeydenlaxharoth@gmail.com>
2024-04-24 13:32:19 -0600

    Update time_measure.h

--- time_measure.h
+++ time_measure.h
@@ -12,8 +12,8 @@
         struct timespec start_0f915e730828c, end_0f915e730828c;             \
         double elapsed_0f915e730828c;                                       \
         double average_time_0f915e730828c = 0;                              \
-        for (size_t index_0f915e730828c = 0;                                \
-             index_0f915e730828c < SAMPLE_SIZE; index_0f915e730828c++) {    \
+        size_t index_0f915e730828c = SAMPLE_SIZE;                           \
+        while (index_0f915e730828c--) {                                     \
             clock_gettime(CLOCK_MONOTONIC, &start_0f915e730828c);           \
             code;                                                           \
             clock_gettime(CLOCK_MONOTONIC, &end_0f915e730828c);             \
@@ -23,7 +23,7 @@
                     1e9;                                                    \
             average_time_0f915e730828c += elapsed_0f915e730828c;            \
         }                                                                   \
-        average_time_0f915e730828c;                                         \
+        average_time_0f915e730828c / SAMPLE_SIZE;                           \
     })
 
 #endif /* TIME_MEASURE_H */

0b53e32d769fa28378f256c831cb29b8bd416e26
Luis Daniel Espino Mandujano <zeydenlaxharoth@gmail.com>
2024-05-15 13:00:02 -0600

    Update cipher.c

--- cipher.c
+++ cipher.c
@@ -1,18 +1,18 @@
 #include "cipher.h"
 
-void cipher(uint64_t *plain_text, uint64_t *const cipher_stream,
-            uint64_t length, uint64_t cipher_stream_size,
-            uint64_t cipher_stream_index, uint8_t *hash_ref,
-            uint64_t aux_renyi_r, uint64_t aux_renyi_j) {
+inline void cipher(uint64_t *plain_text, uint64_t *const cipher_stream,
+                   uint64_t length, uint64_t cipher_stream_size,
+                   uint64_t cipher_stream_index, uint8_t *hash_ref,
+                   uint64_t aux_renyi_r, uint64_t aux_renyi_j) {
     uint64_t sum = 0;
     uint64_t iter;
     uint64_t cipher_text_ant = *cipher_stream;
     uint8_t *sum_bytes = (uint8_t *)&sum;
-    for (iter = 0; iter < length; iter += 2, ++plain_text) {
+    for (iter = 0; iter < length; ++iter, ++plain_text) {
         const uint64_t cipher_stream_current_index =
-            (cipher_stream_index + iter + 1) % cipher_stream_size;
+            (cipher_stream_index + iter * 2 + 1) % cipher_stream_size;
         const uint64_t cipher_stream_current_index_next =
-            (cipher_stream_index + iter + 2) % cipher_stream_size;
+            (cipher_stream_index + iter * 2 + 2) % cipher_stream_size;
         const uint64_t num_1 = cipher_stream[cipher_stream_current_index];
         const uint64_t num_2 = cipher_stream[cipher_stream_current_index_next];
         const uint64_t renyi_cipher_val =
@@ -35,11 +35,11 @@ void decipher(uint64_t *cipher_text, uint64_t *const cipher_stream,
     uint64_t iter;
     uint64_t cipher_text_ant = *cipher_stream;
     uint8_t *sum_bytes = (uint8_t *)&sum;
-    for (iter = 0; iter < length; iter += 2, ++cipher_text) {
+    for (iter = 0; iter < length; ++iter, ++cipher_text) {
         const uint64_t cipher_stream_current_index =
-            (cipher_stream_index + iter + 1) % cipher_stream_size;
+            (cipher_stream_index + iter * 2 + 1) % cipher_stream_size;
         const uint64_t cipher_stream_current_index_next =
-            (cipher_stream_index + iter + 2) % cipher_stream_size;
+            (cipher_stream_index + iter * 2 + 2) % cipher_stream_size;
         const uint64_t num_1 = cipher_stream[cipher_stream_current_index];
         const uint64_t num_2 = cipher_stream[cipher_stream_current_index_next];
         const uint64_t renyi_cipher_val =

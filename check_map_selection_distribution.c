9220fbd66d86d358ee1af6e5a54915672efab00a
Luis Daniel Espino Mandujano <zeydenlaxharoth@gmail.com>
2024-06-13 11:10:40 -0600

    Update check_map_selection_distribution.c
    Check if distribution is uniform using chi-squared test with significance level of 0.05

--- check_map_selection_distribution.c
+++ check_map_selection_distribution.c
@@ -1,33 +1,82 @@
+#include <stdio.h>
+
+#include "ConfigStructure.c"
 #include "ConfigStructure.h"
 #include "chi_squared.c"
+#include "roulete.c"
 #include "roulete.h"
-#include "stdio.h"
 
 int main(int argc, char const *argv[]) {
-    const size_t stream_size = (size_t)1e3;  // 1 MB
-
+    const size_t total_iterations = (size_t)5e3;
+    const double significance_level = 0.05;
+    uint64_t *seed;
     Configuracion config;
-    readConfigFile("config.txt", &config);
-    struct rouleteConfig roulete_config;
-    initilizale_roulete(&config, &roulete_config);
-
-    double *selection_distribution = malloc(sizeof(double) * config.num_maps);
-    memset(selection_distribution, 0, sizeof(double) * config.num_maps);
+    double *selection_distribution;
     int i = 0;
-    for (i = 0; i < stream_size; i++) {
+    int is_consistent = 0;
+    size_t is_consistent_from = 0;
+
+    readConfigFile("config.txt", &config);
+    if (argc >= 2) {
+        config.num_maps = strtoul(argv[1], NULL, 0);
+    } else {
+        config.num_maps = 8;
+    }
+
+    seed = malloc(config.num_maps * sizeof(uint64_t) * 3);
+    generate_seed(&config, seed);
+
+    struct rouleteConfig roulete_config;
+    initilizale_roulete(&config, &roulete_config, seed);
+
+    selection_distribution = malloc(sizeof(double) * config.num_maps);
+    memset(selection_distribution, 0, sizeof(double) * config.num_maps);
+
+    for (i = 0; i < 2000; i++) {
         selection_distribution[roulete_config.roulette_selector %
                                config.num_maps]++;
         random_select_coupled_chaotic_map_lookuptable(&roulete_config);
-        printf("iteration %07d is uniform: %f\n", i,
-               chiIsUniform(selection_distribution, config.num_maps, 0.05));
+        double p_value = chiIsUniform(selection_distribution, config.num_maps,
+                                      significance_level);
+        if (p_value <= significance_level) {
+            is_consistent = 0;
+        } else {
+            if (!is_consistent) {
+                is_consistent = 1;
+                is_consistent_from = i + 1;
+            }
+        }
     }
-
+    for (; i < total_iterations; i++) {
+        selection_distribution[roulete_config.roulette_selector %
+                               config.num_maps]++;
+        random_select_coupled_chaotic_map_lookuptable(&roulete_config);
+        double p_value = chiIsUniform(selection_distribution, config.num_maps,
+                                      significance_level);
+        if (p_value <= significance_level) {
+            is_consistent = 0;
+        } else {
+            if (!is_consistent) {
+                is_consistent = 1;
+                is_consistent_from = i + 1;
+            }
+        }
+        printf("iteration %07d is uniform?: %f, %c\n", i + 1, p_value,
+               (p_value > significance_level) ? 'T' : 'F');
+        ;
+    }
+    printf("significance_level: %f\n", significance_level);
+    if (is_consistent)
+        printf("Passes hypothesis test from: %d", is_consistent_from);
+    else
+        printf("does not pass hypothesis test");
     FILE *stream = fopen("distribution.txt", "w");
     for (int i = 0; i < config.num_maps; i++)
-        printf(" %f\n", selection_distribution[i]);
+        fprintf(stream, "%f\n", selection_distribution[i]);
     fclose(stream);
 
     free(selection_distribution);
+    free(seed);
     delete_roulete(&roulete_config);
 
     return 0;
